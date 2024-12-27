56088c10b02c2f3b2bb9547e723abdbc2b24ec2a
Boualili Youcef <youcefboualili0@gmail.com>
2024-12-27 23:56:52 +0100

    Chess Game

---
+++ Chess-Game-master/README.md
@@ -0,0 +1,54 @@
+# Chess-Game
+
+ This project is the C++ implementation of Player vs Player Chess Game in C++ using SFML graphics Library. 
+
+ ![Sample Image](a.png)
+
+ ## Setting up SFML on Linux
+
+ ```
+ $ sudo apt-get install libsfml-dev
+ ```
+ 
+## Steps to run this program
+
+1. Clone the repository:
+   ```
+   $ git clone https://github.com/boualili/Chess-Game.git
+   ```
+
+2. Navigate to the project directory:
+   ```
+   $ cd Chess-Game
+   ```
+
+3. Open the `src` directory:
+   ```
+   $ cd src
+   ```
+
+4. Compile the program:
+   ```
+   $ g++ -o chess main.cpp -lsfml-graphics -lsfml-window -lsfml-system
+   ```
+
+5. Run the program:
+   ```
+   $ ./chess
+   ```
+
+## Project Structure
+
+- `src/`: Contains the source code files.
+- `include/`: Contains the header files.
+- `assets/`: Contains the images and other assets used in the game.
+
+## Dependencies
+
+- SFML (Simple and Fast Multimedia Library)
+
+## License
+
+This project is licensed under the MIT License.
+
+
---
+++ Chess-Game-master/build/CMakeFiles/ChessGame.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChessGame.dir/src/Bishop.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Bishop.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/Game.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Game.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/King.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/King.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/Knight.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Knight.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/Pawn.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Pawn.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/Pieces.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Pieces.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/Queen.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Queen.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/Rook.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Rook.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/Square.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/Square.cpp.o.d"
+  "CMakeFiles/ChessGame.dir/src/main.cpp.o"
+  "CMakeFiles/ChessGame.dir/src/main.cpp.o.d"
+  "ChessGame"
+  "ChessGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChessGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
